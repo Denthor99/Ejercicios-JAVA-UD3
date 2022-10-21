6157edcf53fa159c8fd53a6d45679e855b65fe44
Den <drodsan2708@g.educaand.es>
2022-10-21 14:34:53 +0200

    Create IfParimpar.java
    Segundo ejercicio, ParImpar usando un If

---
+++ IfParimpar.java
@@ -0,0 +1,12 @@
+public class IfParimpar {
+    public static void main(String[] args){
+        int edad=31;
+        int num=edad%2;
+        if (num == 0) {
+            System.out.println(edad + " es par");
+        }else{ 
+            System.out.println(edad + " es impar");
+        }
+    }
+   
+}
