a326b281695aec41411af443a2d79a2cdca22e31
Den <drodsan2708@g.educaand.es>
2022-10-24 13:10:27 +0200

    Ejercicios_clase
    Ejercicios hechos en clase

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,6 @@
-public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println("Adios Mundo");
-        
+public class HolaMundo { //Nombre de la clase
+    public static void main(String[] args) { //Declaración del metodo main (punto de entrada del programa)
+        //Con system.out.println introducimos un conjunto de carácters (string), además de realizar al final un salto de linea
+        System.out.println("Hola Mundo, que tal?."); //
     }
-    
 }
